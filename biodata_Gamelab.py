828567243059d9c30b46ad27c4c30531037f8659
muhammad khairul syabil <137250255+syabil123@users.noreply.github.com>
2023-06-22 14:40:17 +0700

    biodata_Gamelab.py

---
+++ biodata_Gamelab.py
@@ -0,0 +1,13 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "Muhammad Khairul syabil"
+hobi = "Bermain Bola"
+alamat = "Jaali regency blok A5"
+
+print("Nama Lengkap :",Muhammad Khairul Syabil)
+print("alamat       :",Jamali regency, "\n            Kota ", Kota)
+print("Hobi         :", hobi)
+
+print ("=======================================")
